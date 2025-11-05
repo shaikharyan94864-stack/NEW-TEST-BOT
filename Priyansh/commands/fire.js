250657ea6eb713f1e0aaee17b4ea933c4e872f03
shaikharyan94864-stack <shaikharyan94864@gmail.com>
2025-11-05 23:22:05 +0530

    Update fire.js

--- Priyansh/commands/fire.js
+++ Priyansh/commands/fire.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "Fire",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐑𝐚𝐣𝐚",
     description: "Fire broke the boxchat",
     commandCategory: "group",
     usages: "bold Fire",
@@ -20,8 +20,8 @@ module.exports.run = async function({ api, args, Users, event}) {
     var arraytag = [];
         arraytag.push({id: mention});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-a("T3R9 B99P BOT 3NT3R S9L3 APN3 B99P S3 JUB99N L9D9Y3G9🤞🏻👅👅");
-setTimeout(() => {a({body: " BOT KO G9LI D3N3 W9LO TUMH9RII B9HN KII (+)🙊💋" })}, 3000);
+a("T3R9 B99P R9J9 3NT3R S9L3 APN3 B99P S3 JUB99N L9D9Y3G9🤞🏻👅👅");
+setTimeout(() => {a({body: " R9J9 KO G9LI D3N3 W9LO TUMH9RII B9HN KII (+)🙊💋" })}, 3000);
 setTimeout(() => {a({body: " T3RII M99 BHOSD99 F99D KR F3KH DUNG9 S9L33 H9WB99Z T9TT3💋🤣"})}, 5000);
 setTimeout(() => {a({body: " T3RII M99 K3 BHOSD33 M3 MOOT DUNG99 M9RCHOXDD😜💔" })}, 7000);
 setTimeout(() => {a({body: " T3RII M9 KI (+) R9ND K3 B9XH33🤚🏻💋" })}, 9000);
@@ -46,7 +46,24 @@ setTimeout(() => {a({body: "M9DRCHOD H9W99B9Z T9TT3 COM3DY KR RH9😂😂" })}, 
 setTimeout(() => {a({body: "T3RI B9HN KI B9CHH3D9NNNI M3 LUND D3KR USKI B9CH9D9NNI F99D DUNG99💋💋" })}, 100000);
 setTimeout(() => {a({body: " T3RII M99 KI S9DII XHUTT PR NIMMBU NICHOD KR RUS NIK9LUNG9💆‍♂️😈❤" })}, 105000);
 setTimeout(() => {a({body: " T3RI B9HN KI CHOTI B9DI BOOBS PR LOD99 F3KH KR M9RUNG9 R9NDII K3😈💋"})} , 115000);
-setTimeout(() => {a({body: "OK T3R9 B99P J9 RH9 9B RON9 M9T DON 3XII7😈💋"})} , 120000);
-
+setTimeout(() => {a({body: " TER1 BEHEN K1 CHOOT TO K4L4P K4L4P KE LOWD4 CHUSE J44 RH1 H41 HEN HEN BEHENCHOD KE D1NNE =]] 
+setTimeout(() => {a({body: " BHOSD1KE TER1 BEHEN KE BOOR KO M41 CHEER J4UNG4 LOWDE KE D1NNE TER1 BEHEN K1 CHOOT KO M41 M4RTE J4U BEHENCHOD KE B4CCHWW =]] "})} , 120500);
+setTimeout(() => {a({body: " R4ND1 KE 4UL44D TU KREG4 B44P SE F4D44 4J44 BE LOWDE TER1 BEHEN K1 CHOOT KO TO M44R H1 D1Y4 HUN =]] "})} , 115000);
+setTimeout(() => {a({body: " N4W TO M41 1TN4 K4L4P44 DUNG4 K1 TER1 BEHEN K1 CHOOT RO RO KE MERE LOWDE KO CHUSTE J44YEG1 =]] "})} , 115000);
+setTimeout(() => {a({body: " TER1 BEHEN K1 CHOOT KO M41 44J RUL4 D1Y4 HUN HEHEHEHEH TU B44P SE L4DEG4 HEHEHEHEHE GH4ST1 KE B4CHEW :v 44J4 M44 KE LOWDE <3
+setTimeout(() => {a({body: " BEHENCHOD KE LOWDE R4ND1 1NSH44N KE R4ND1 B4CHEW TER1 BEHEN K1 CHOOT KO M41 CHODTE J4UNG4 =]] "})} , 1115000);
+setTimeout(() => {a({body: " TER1 M44 K1 CHOOT KO M41 M4R M44R KE TUJH J41SE 4NP4DH G44W444R KO M41 L1KHN4 S1KH4 DUNG4 LOWDE KE B44L =]] "})} , 115000); setTimeout(() => {a({body: "OK44T LESSS B4CHEW B|"})} , 115000);
+setTimeout(() => {a({body: "LOWDE KEE B4CHEW TER1 BEHEN K1 CHOOT KO M41 CHOD KR N44SH KR DU LOWDE KE B4CHEW =]] "})} , 115000);
+setTimeout(() => {a({body: " TER1 BEHEN KE BHOSDE KO M41 CHOD D4LU BEHENCHOD KE LOWDE TER1 BEHEN KE BHOSDE KO M41 CHODTE J4U =]] "})} , 115000);
+setTimeout(() => {a({body: "LOWDE KE D1NNE TER1 BEHEN K1 G44ND KO M41 CHEER J4U BEHENCHOD KE LOWDE R4ND1 KE P1LLEY TER1 BEHEN K1 G44ND M4RU M44 KE LOWDE =]] "})} , 115000);
+setTimeout(() => {a({body: "TO MERE BETE MER4 LOWD4 FELL KR RH4 H41 N4W 4CHEW SE H4WN BEHENCHOD KE D1NNE TER1 BEHEN K1 CHOOT TO ROTE J44YEG1 LOWDE <3"})} , 115000);
+setTimeout(() => {a({body: " TER1 D444D11 K1 CHOOT ME M41 LED1 K44 TEL L4G4 KE M41 41S4 M44R M4RUNG4 K1 TER1 D44D11 K1 CHOOT K4L4P UTHEG1 BEHENCHOD KE B4CHEW HEHEHEHEH <3"})} , 115000);
+ setTimeout(() => {a({body: "TER1 M44 KE KUSS KO M41 CHEER DU SU4R KE B4CHEW TER1 BEHEN KE BHOSDE M4 M14 T4B4H1 KRU =]] "})} , 115000);
+setTimeout(() => {a({body: " TER1 BEHEN KE BHOSDE KO M41 CHODTE J4UNG4 R4ND1 KE 4UL44D TER1 BEHEN K1 CHOOT ME M41 LOWD4 DUNG4 BEHENCHOD KE B4CHEW TER1 BEHEN K1 CHOOT M4RUNG4 BHOSDKE B4CHEW =]] "})} , 115000);
+ setTimeout(() => {a({body: "TER1 M44 K1 CHOOT DEKH 44J K41SE K4PT1 H41 BEHENM48HOD KE B4CHEW TU RO RO KE MERE LOWDE K4 44J BH1KH M44NG BEHENCHOD KE B4CHEW =]] "})} , 115000);
+ setTimeout(() => {a({body: "TER1 M44 KE BHOSDE M4 M41 L44T M44R K4R TER1 BEHEN KE BHOSDE KO M41 CHEER DUNG4 <3 =]]"})} , 115000);
+setTimeout(() => {a({body: "SUN4 H41 TER1 B44J1 CHOOT M41 DOODH D44LO TOU P4N1 N1K4LT4 H41 YE S4CH H41 KY4 ?"})} , 115000);
+ setTimeout(() => {a({body: "TER1 BEHEN K1 CHOOT ME M41 LOWD4 DUNG4 BEHENCHOD KE B4CHEW R4ND1 KE D1NNE LOWDE KE B4CHEW =]] "})} , 115000);
+setTimeout(() => {a({body: "BEHENCHOD KE D1NNE TER1 BEHEN K1 G44ND ME M41 41S4 LOWD4 D4LU K1 TER1 BEHEN K1 CHOOT P4G4L HO J44YEG1 =]] "})} , 120000);
   
 }
